5020b8a7ddd842db64217b18f447fdb58a770511
MBarzallo <146205152+MBarzallo@users.noreply.github.com>
2025-01-15 08:51:50 -0500

    Cambios mensaje tabla

--- src/main/java/ec/edu/ups/ppw/WSParkingRamirezBarzallo/model/Message.java
+++ src/main/java/ec/edu/ups/ppw/WSParkingRamirezBarzallo/model/Message.java
@@ -1,4 +1,46 @@
 package ec.edu.ups.ppw.WSParkingRamirezBarzallo.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="id_message")
+    private int idMessage;
+    private String message;
+    private String email;
+    private String name;
+
+    public int getIdMessage() {
+        return idMessage;
+    }
+
+    public void setIdMessage(int idMessage) {
+        this.idMessage = idMessage;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
