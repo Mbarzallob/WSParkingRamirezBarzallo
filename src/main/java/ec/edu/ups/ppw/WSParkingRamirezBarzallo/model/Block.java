410ff2614c61437d73066220a8e5c9984b5e8774
MBarzallo <146205152+MBarzallo@users.noreply.github.com>
2025-01-20 07:56:39 -0500

    Aumentar controllers

--- src/main/java/ec/edu/ups/ppw/WSParkingRamirezBarzallo/model/Block.java
+++ src/main/java/ec/edu/ups/ppw/WSParkingRamirezBarzallo/model/Block.java
@@ -1,5 +1,30 @@
 package ec.edu.ups.ppw.WSParkingRamirezBarzallo.model;
 
-public class Block {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Block {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/ec/edu/ups/ppw/WSParkingRamirezBarzallo/model/ParkingSpace.java
+++ src/main/java/ec/edu/ups/ppw/WSParkingRamirezBarzallo/model/ParkingSpace.java
@@ -1,4 +1,30 @@
 package ec.edu.ups.ppw.WSParkingRamirezBarzallo.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class ParkingSpace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "block_id")
+    private Block block;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Block getBlock() {
+        return block;
+    }
+
+    public void setBlock(Block block) {
+        this.block = block;
+    }
 }
