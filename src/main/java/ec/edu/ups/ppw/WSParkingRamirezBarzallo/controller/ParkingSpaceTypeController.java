60f6ceec8b195fb8177773f516974193d08b2eb1
jennyfer04ramirez <jr.6311.camm@gmail.com>
2025-01-20 08:24:06 -0500

    Changes ParkingSpaceType

--- src/main/java/ec/edu/ups/ppw/WSParkingRamirezBarzallo/controller/ParkingSpaceTypeController.java
+++ src/main/java/ec/edu/ups/ppw/WSParkingRamirezBarzallo/controller/ParkingSpaceTypeController.java
@@ -1,5 +1,29 @@
 package ec.edu.ups.ppw.WSParkingRamirezBarzallo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ec.edu.ups.ppw.WSParkingRamirezBarzallo.model.ParkingSpaceType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/parkingSpaceType")
 public class ParkingSpaceTypeController {
+
+	@GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getParkingSpaceType(){
+        try
+        {
+            List<ParkingSpaceType> parkingSpaceType = new ArrayList<ParkingSpaceType>();
+            return Response.ok(parkingSpaceType).build();
+        }
+        catch(Exception e){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
 
 }
